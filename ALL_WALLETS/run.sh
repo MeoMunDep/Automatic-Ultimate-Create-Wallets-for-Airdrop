5ac7c4f0982108560f757ac9fd26e832c9c37595
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-04-26 16:05:50 +0700

    shorten module

--- ALL_WALLETS/run.sh
+++ ALL_WALLETS/run.sh
@@ -9,7 +9,7 @@ echo -e "        Wallet Generator by MeoMunDep        "
 echo -e "=============================================${NC}"
 echo
 echo -e "${GREEN}Installing necessary modules...${NC}"
-npm install @aptos-labs/ts-sdk @hashgraph/sdk @mysten/sui @polkadot/keyring @polkadot/util-crypto @solana/web3.js @stablelib/ed25519 @ton/crypto algosdk bech32 bip32 bip39 bitcoinjs-lib blake2b blakejs bs58check cardano-wallet-js colors ed25519-hd-key ethers ethereumjs-util hdkey near-api-js ton ton-core ton-crypto tonweb tiny-secp256k1 tronweb secp256k1 xlsx --no-audit --prefer-offline --legacy-peer-deps --save-exact --force
+npm install --no-audit --prefer-offline --legacy-peer-deps --save-exact --force
 echo
 echo -e "${GREEN}Starting the bot...${NC}"
 cd "$(dirname "$0")"
